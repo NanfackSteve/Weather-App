efc73db1407ac39b7f4291e62f0184ed51844cfa
nanfack steve <nanfacksteve7@gmail.com>
2021-03-27 12:04:45 +0000

    Design ain page

--- weatherApp.py
+++ weatherApp.py
@@ -3,19 +3,18 @@
 import json, sys
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.network.urlrequest import UrlRequest
 from kivy.uix.recycleview import RecycleView
 
-class AddLocationForm(BoxLayout):
+class WeatherRoot(FloatLayout):
     search_input = ObjectProperty()
-    search_results = ObjectProperty()
     appId = 'abdbe414ad18557599a674e8bd94f8fd'
     #Op3nW34th3r
 
     def search_location(self):
-        search_template = "http://api.openweathermap.org/data/2.5/" + "weather?q={}&APPID=" + self.appId
-        search_url = search_template.format(self.search_input.text)
+        search_url = f"http://api.openweathermap.org/data/2.5/weather?q={self.search_input.text}&APPID=" + self.appId
         request = UrlRequest(search_url, self.found_location)
         print("Done ", request)
 
@@ -23,8 +22,12 @@ class AddLocationForm(BoxLayout):
         weather_datas = [{'text':"{} ({})".format(data['name'], data['sys']['country'])}]
         print(weather_datas)
         if( len(weather_datas) != 0):
-            self.search_results.data.insert(0,weather_datas[0])
+            pass
+            #self.search_results.data.insert(0,weather_datas[0])
         self.search_input.text = ""
+
+class HistoryForm(BoxLayout):
+    search_results = ObjectProperty()
 
 class RecyclerView(RecycleView):
     def __init__(self, **kwargs):
@@ -35,9 +38,7 @@ class WeatherApp(App):
 
     title = "My Weather App "
     def build(self):
-        return AddLocationForm()
-
-
+        return WeatherRoot()
 
 if __name__ == '__main__':
     WeatherApp().run()
